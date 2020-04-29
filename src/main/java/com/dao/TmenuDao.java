0e16d389f2c36b94c9c46ee47cc52ee39df02552
朱梅 <1540588534@qq.com>
2020-04-30 02:41:21 +0800

    修改了sql语句中中文符号？

--- src/main/java/com/dao/TmenuDao.java
+++ src/main/java/com/dao/TmenuDao.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class TmenuDao {
     public List getByUid(String uid){
 //        使用的是inner json,根据tmenu，trm，tur(rid)的id来查询，返回tmenu对象
-        String sql="SELECT tmenu.* FROM tmenu INNER JOIN trm ON tmenu.id=trm.id INNER JOIN tur ON tmenu.id=tur.rid WHERE tur.uid=？";
+        String sql="SELECT tmenu.* FROM tmenu INNER JOIN trm ON tmenu.id=trm.id INNER JOIN tur ON tmenu.id=tur.rid WHERE tur.uid=?";
         return DH.getall(sql,new Tmenu(),new String[]{uid});
     }
 }
