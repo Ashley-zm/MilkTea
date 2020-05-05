cd3e1ddd75c22820ea63856a5a024e6a7123dae9
朱梅 <1540588534@qq.com>
2020-05-05 10:03:06 +0800

    使用DbUtils的new ScalarHandler()返回聚合函数查询的结果集（不需要传参）

--- src/main/java/com/dao/DH.java
+++ src/main/java/com/dao/DH.java
@@ -10,7 +10,9 @@ import java.util.List;
 
 import org.apache.commons.dbutils.DbUtils;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
 
 public class DH {
 
@@ -18,7 +20,7 @@ public class DH {
 	public static final String dr = "jdbc:mysql://localhost:3306/naicha?&useSSL=false&serverTimezone=UTC";
 	public static final String uid = "root";
 	public static final String pwd = "password";
-
+//查询（多、一）
 	public static List getall(String sql, Object o, String[] args) {
 		List li = new ArrayList();
 		Connection conn = null;
@@ -38,7 +40,7 @@ public class DH {
 
 		return li;
 	}
-
+//增加、删除、修改
 	public static int update(String sql, String[] args) {
 		int num = 0;
 		Connection conn = null;
@@ -57,6 +59,26 @@ public class DH {
 
 		}
 		return num;
+	}
+	//new ScalarHandler()通常用在聚合函数查询的结果集处理，对单列单行进行处理
+	public static Number getcount(String sql) {
+		Number num=0;
+		Connection conn = null;
+		DbUtils.loadDriver(DH.connstr);
+		try {
+			conn = DriverManager.getConnection(DH.dr, DH.uid, DH.pwd);
+			QueryRunner qr = new QueryRunner();
+			num = (Number)qr.query(conn, sql, new ScalarHandler());
+			 System.out.println("总数据"+num.intValue());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+
+			DbUtils.closeQuietly(conn);
+
+		}
+		return num.intValue();
 	}
 
 	public static Connection getconn() {
--- src/main/java/com/web/SelectGoodServlet.java
+++ src/main/java/com/web/SelectGoodServlet.java
@@ -1,5 +1,7 @@
 package com.web;
 
+import com.service.GoodsService;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,8 +9,27 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class SelectGoodServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    private static final long serialVersionUID = 1L;
+    private GoodsService tl = new GoodsService();
 
+    public SelectGoodServlet() {
+        super();
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setCharacterEncoding("utf-8");
+        String sname = request.getParameter("sname");
+        String materials = request.getParameter("materials");
+        System.out.println("*****************"+sname);
+        System.out.println(materials+"*****************");
+//        处理业务
+        String s = tl.select(sname, materials);
+//        String s = tl.select("巧克力", "巧克力");
+        System.out.println("============" + s + "===============");
+//        输出
+        //我这里是返回 json 类型
+        response.getWriter().print(s);
+//        JSONObject jsonObject = JSONObject.parseObject(s);
+//        System.out.println("============" + jsonObject.values() + "===============");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
