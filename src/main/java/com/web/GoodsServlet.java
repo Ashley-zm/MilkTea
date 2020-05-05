61af4fb03ddc8782c93f36c0166ceee75ce19190
朱梅 <1540588534@qq.com>
2020-05-05 23:18:42 +0800

    重写了查询商品接口（获取了layui的page，limit传递到new Page

--- src/main/java/com/web/GoodsServlet.java
+++ src/main/java/com/web/GoodsServlet.java
@@ -1,9 +1,10 @@
 package com.web;
 
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
+//import com.alibaba.fastjson.JSONObject;
 import com.service.GoodsService;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,10 +12,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import  com.pojo.Page;
 //web也就是controller
 public class GoodsServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private GoodsService tl = new GoodsService();
+    private GoodsService ct = new GoodsService();
 
     public GoodsServlet() {
         super();
@@ -22,21 +25,39 @@ public class GoodsServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String l=tl.getall();
         response.setCharacterEncoding("utf-8");
         response.setContentType("text/html");
+
+        int page1 = Integer.parseInt(request.getParameter("page"));
+        int limit = Integer.parseInt(request.getParameter("limit"));
+        System.out.println("page1------------------"+page1);
+        System.out.println("limit*******************"+limit);
+/***
+ *total   总记录数
+ *currentpage    当前页
+ *pagesize    每页显示多少条
+ *public Page(int total,int currentpage,int pagesize)
+ *Page page = new Page(pageCount, currentPage, pageSize);
+  */
+        Page page=new Page(tl.getTotalCount(),page1,limit);
+        System.out.println("page------------------"+page);
+        int totalCount = ct.getTotalCount();
+        String cout=String.valueOf(totalCount);
+        request.setAttribute("count",cout);
+
+//        //查询所有商品
+        String l=tl.getall(page);
+
         PrintWriter out=response.getWriter();
-//        把json字符串转换成json对象
-//        JSONObject lj = JSON.parse(l);
-//        JSONArray listArray=JSONArray.fromObject(lists);
         System.out.println("--------------"+l);
         out.println(l);
 //        flush()意思是把缓冲区的内容强制的写出
 //        flush方法一般是程序写入完成时执行。随后跟着close方法。
         out.flush();
         out.close();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
     }
 }
