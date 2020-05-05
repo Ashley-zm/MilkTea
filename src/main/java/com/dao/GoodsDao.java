9d3ad82bf8664e555f52ae2ec438ab4d6c4dd54e
朱梅 <1540588534@qq.com>
2020-05-05 23:17:07 +0800

    在getall中添加了Page、删除商品、获得总商品数量

--- src/main/java/com/dao/GoodsDao.java
+++ src/main/java/com/dao/GoodsDao.java
@@ -1,14 +1,24 @@
 package com.dao;
 
 import com.pojo.Goods;
+import com.pojo.Page;
 
 import java.util.List;
 
 public class GoodsDao {
-    public List getall(){
-        String sql="select * from goods";
+//    查询所有商品
+    public List getall(Page page){
+        //start：从那条开始查，size：每页查询的数量
+        //报错：start和size是私有的
+        String sql="select * from goods limit "+page.start+","+page.size;
         return DH.getall(sql,new Goods(),new String[] {});
     }
+//    模糊查询
+    public List select(String sname,String materials){
+            String sql=" SELECT * FROM goods WHERE sname LIKE \"%\"?\"%\" OR materials LIKE \"%\"?\"%\" ";
+        return DH.getall(sql,new Goods(),new String[] {sname,materials});
+    }
+//    插入商品
     public Boolean save(String sname,String sprice,String materials,String introduction){
         String sql="insert into goods(sname ,sprice,materials,introduction) values (?,?,?,?)";
         int num = DH.update(sql, new String[]{sname, sprice, materials, introduction});
@@ -19,6 +29,7 @@ public class GoodsDao {
             return false;
         }
     }
+//    更新商品
     public Boolean update(String sname,String sprice,String materials,String introduction,String id){
         String sql="update goods set sname=?,sprice=?,materials=?,introduction=? where id=?";
         int num = DH.update(sql, new String[]{sname, sprice, materials, introduction,id});
@@ -30,4 +41,25 @@ public class GoodsDao {
         }
 
     }
+//    删除商品
+public Boolean delete(String sname,String sprice,String materials,String introduction,String id){
+    String sql="update goods set sname=?,sprice=?,materials=?,introduction=? where id=?";
+    int num = DH.update(sql, new String[]{sname, sprice, materials, introduction,id});
+    if(num>0){
+        return true;
+    }
+    else {
+        return false;
+    }
+
+}
+//    获取所有数据总数
+    //报错Number无法转换成int--修改.intValue()
+    public int getTotalCount(){
+        String sql=" SELECT COUNT(*) FROM goods";
+        int count=DH.getcount(sql).intValue();
+        return count;
+
+    }
+
 }
